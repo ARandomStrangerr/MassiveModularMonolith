67cd81f92ca601525eba093da274b973301526fe
microbe-obliterator <billslim0996@gmail.com>
2022-12-02 03:32:59 -0800

    shorten the code

--- ConnectionManager/src/chain/connection_manager/request_handler/LinkSearchMacAddress.java
+++ ConnectionManager/src/chain/connection_manager/request_handler/LinkSearchMacAddress.java
@@ -15,11 +15,10 @@ public class LinkSearchMacAddress extends Link {
     public boolean execute() {
         String mac = chain.getProcessObject().get("macAddress").getAsString().toLowerCase();
         try {
-            ConnectionManager.getInstance().database.getLoginInfo(mac);
+            return ConnectionManager.getInstance().database.getLoginInfo(mac);
         } catch (SQLException e){
             e.printStackTrace();
             return false;
         }
-        return false;
     }
 }
