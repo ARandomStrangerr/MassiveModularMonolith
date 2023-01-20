cca7580d636c1503a61be67c6f9062b55a0074b8
microbe-obliterator <billslim0996@gmail.com>
2023-01-20 02:52:55 -0800

    mix

--- ConnectionManager/src/ConnectionManager.java
+++ ConnectionManager/src/ConnectionManager.java
@@ -7,11 +7,12 @@ import java.net.InetAddress;
 
 public class ConnectionManager {
     public static void main(String[] args) {
+        String str = "{\"moduleName\":\"ConnectionManager\",\"hostAddress\":\"localhost\",\"hostPort\":9999,\"listenerPort\":9998,\"keyStorePath\":\"key.jks\",\"keyStorePassword\":\"hungcom23\",\"databaseUrl\":\"jdbc:postgresql://localhost:5432/authentication?password=hungcom23\",\"poolSize\":2,\"timeout\":5000}";
         if (args.length == 0) {
             System.err.println("Missing the argument");
             return;
         }
-        JsonObject information = new Gson().fromJson(args[0], JsonObject.class);
+        JsonObject information = new Gson().fromJson(str, JsonObject.class);
         if (!new ChainStartModule(information).execute()) System.exit(2);
     }
 }
@@ -26,7 +27,7 @@ class TestCase1 {
 
 class SMTPMailTester {
     public static void main(String[] args) throws Exception {
-        SocketWrapper socketWrapper = new SocketWrapper(InetAddress.getByName("localhost"), 9998, "key.jks", "hungcom23");
+        SocketWrapper socketWrapper = new SocketWrapper(InetAddress.getByName("118.70.127.55"), 8889, "key.jks", "hungcom23");
         socketWrapper.write("{macAddress:\"24:41:8C:06:2C:82\"}");
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("job", "SMTPMail");
