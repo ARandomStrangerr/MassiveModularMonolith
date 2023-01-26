583f6d3f0cc5306a831e9ab5f1d8edb59005bfc6
microbe-obliterator <billslim0996@gmail.com>
2023-01-26 04:47:55 -0800

    add comment

--- ShareResources/src/database/ConnectionPool.java
+++ ShareResources/src/database/ConnectionPool.java
@@ -2,11 +2,11 @@ package database;
 
 import java.sql.*;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public abstract class ConnectionPool {
     private final LinkedList<Connection> connectionPool;
     private final LinkedList<ConnectionPool> queue;
-
     public ConnectionPool(String url, int poolSize) throws SQLException {
         connectionPool = new LinkedList<>();
         queue = new LinkedList<>();
@@ -26,14 +26,25 @@ public abstract class ConnectionPool {
     }
 
     private synchronized Connection getConnection() throws InterruptedException {
+        /*
+        todo resolve this problem.
+        it is quite strange when one thread return connection notify another thread,
+        it has 0 available connection in the pool when there is supposed to be 1.
+        it is supposed to be:
+        0 connection -> a thread return connection ( 1 connection ) -> notify the next thread ( 1 connection )
+        -> the next thread claim the connection ( 0 connection )
+        instead :
+        0 connection -> a thread return connection ( 1 connection ) -> ? something happened here ( 0 connection )
+        -> notify the next thread ( 0 connection ) -> the next thread claim the connection ( 0 connection ) ERROR !
+         */
         while (true) {
+        if (connectionPool.size() == 0 || queue.size() != 0) {
+            queue.add(this);
+            wait();
+        }
             try {
-                if (connectionPool.size() == 0 || queue.size() != 0) {
-                    queue.add(this);
-                    wait();
-                }
                 return connectionPool.removeFirst();
-            } catch (NullPointerException e){
+            } catch (NoSuchElementException e){
                 continue;
             }
         }
