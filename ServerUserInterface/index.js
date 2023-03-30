e6245d3e084e0a3a15cb0f9761f39818f58f978e
microbe-obliterator <billslim0996@gmail.com>
2023-03-29 20:36:54 -0700

    new project ui application for the server.

---
+++ ServerUserInterface/index.js
@@ -0,0 +1,32 @@
+const electronModule = require("electron");
+const electron = electronModule.app;
+
+let mainWindow;
+let remote;
+function createWindow(){
+	mainWindow = new electronModule.BrowserWindow(
+		{
+			webPreferences:
+			{
+				nodeIntegration: true,
+				contextIsolation: false,
+			},
+			width: 1600,
+			height: 1000
+		}
+	);
+	mainWindow.loadURL(`file://${__dirname}/MainPage.html`);
+		// mainWindow.webContents.openDevTools();
+		mainWindow.on("closed",
+		function(){
+			mainWindow = null;
+			remote = null;
+		}
+	);
+}
+
+electron.on("ready", createWindow);
+electron.on("window-all-closed",
+function() {
+	electron.quit();
+});
