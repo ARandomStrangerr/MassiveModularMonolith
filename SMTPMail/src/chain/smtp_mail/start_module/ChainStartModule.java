b651f2d90ce7422371959c49f7886631bd97f2a8
microbe-obliterator <billslim0996@gmail.com>
2023-04-14 18:44:37 -0700

    change due to the new structure of Chain super class

--- SMTPMail/src/chain/smtp_mail/start_module/ChainStartModule.java
+++ SMTPMail/src/chain/smtp_mail/start_module/ChainStartModule.java
@@ -6,7 +6,7 @@ import com.google.gson.JsonObject;
 public class ChainStartModule extends Chain {
     public ChainStartModule(JsonObject processObject) {
         super(processObject);
-        chain.add(new LinkSetInfo(this));
-        chain.add(new LinkStartSocket(this));
+        super.addLink(new LinkSetInfo(this),
+            new LinkStartSocket(this));
     }
 }
