b524e96be9532aad0aa55bc802afdc1824f82ada
microbe-obliterator <billslim0996@gmail.com>
2023-05-01 15:46:43 -0700

    change monitor port operating. change display message. add function when a new module connect. add comment

--- ServerUserInterface/script/DataStreamPageControl.js
+++ ServerUserInterface/script/DataStreamPageControl.js
@@ -2,9 +2,40 @@ module.exports = class {
   #logUnorderedList;
   constructor(logUnorderedList){
     this.#logUnorderedList = logUnorderedList;
+		document.querySelector("#start-data-stream-module-button").addEventListener("click", () => this.#startModule());
   }
 
-  startModule(javaClassPath, port, jksPath, jksPassword, timeout){
+	#startModule(){ // action starting datastream module
+		let javaClassPath = document.querySelector("#data-stream-module-classpath-input").value;
+		if (javaClassPath === "") {
+			this.#addInvalidLog("Địa điểm tệp tin khởi động chưa được điền");
+			return;
+		}
+		let port = document.querySelector("#data-stream-module-port-input").value;
+		if (port === "") {
+			this.#addInvalidLog("Cổng hoạt động của chưa được điền");
+			return;
+		}
+		let monitorToolPort = document.querySelector("#data-stream-module-monitor-port-input").value;
+		if (monitorToolPort === ""){
+			this.#addInvalidLog("Cổng triết xuất thông tin chưa được điền");
+			return;
+		}
+		let jksPath = document.querySelector("#data-stream-module-jks-input").value;
+		if (jksPath === ""){
+			this.#addInvalidLog("Địa điểm tệp tin bảo mật JKS chưa được điền");
+			return;
+		}
+		let jksPassword = document.querySelector("#data-stream-module-jks-password-input").value;
+		if (jksPassword === ""){
+			this.#addInvalidLog("Mật khẩu của tệp tin bảo mật JKS chưa được điền");
+			return;
+		}
+		let timeout = document.querySelector("#data-stream-module-timeout-input").value;
+		if (timeout === ""){
+			this.#addInvalidLog("Thời gian ngắt cho kết nối chưa được uỷ quyền chưa được điền");
+			return;
+		}
     let childProcess = require("child_process");
     let param = {
       port: port,
@@ -12,7 +43,7 @@ module.exports = class {
       keyStorePath: jksPath,
       keyStorePassword :jksPassword,
       moduleName: "DataStream",
-      monitorToolPort: 9997
+      monitorToolPort: monitorToolPort
     }
     let temp = javaClassPath.split("/");
     temp = temp[temp.length-1];
@@ -23,23 +54,24 @@ module.exports = class {
 
     let socket;
     setTimeout(() => {
-      socket = new (require('./io.js')).Socket("127.0.0.1", 9997);
+      socket = new (require('./io.js')).Socket("127.0.0.1", monitorToolPort);
       socket.setOnDataReceive((data)=>this.#onDataEvent(data)); // f*** this but where does this points to? i don't f***ing know :)
     }, 1000);
   }
 
-  #onDataEvent(data){
+  #onDataEvent(data){ // things to do when information arrive
+		console.log(data);
     try{
       data = JSON.parse(data);
     } catch (e) {
       console.log(e);
       return;
     }
-    let printoutData = `${data.time} ${data.status} ${data.notification}`;
+    let printoutData = `${data.time} ${data.status? "Thành công" : "Lỗi" } ${data.notification}`;
     this.#addValidLog(printoutData);
   }
 
-  #addValidLog(msg){
+  #addValidLog(msg){ // add a message to log section with a green dot
     let listItem = document.createElement("li");
     listItem.classList.add("dot");
     listItem.classList.add("valid-dot");
@@ -47,11 +79,21 @@ module.exports = class {
     this.#logUnorderedList.appendChild(listItem);
   }
 
-  #addInvalidLog(msg){
+  #addInvalidLog(msg){ // add a message to log section with a red dot
     let listItem = document.createElement("li");
     listItem.classList.add("dot");
     listItem.classList.add("invalid-dot");
     listItem.innerText = msg;
     this.#logUnorderedList.appendChild(listItem);
   }
+
+	#addActiveModule(moduleName){ // add an module into active module section with a green dot
+		let moduleNameSpan = document.createElement("span");
+		moduleNameSpan.innerText = moduleName;
+		let disconnectButton = document.createElement("i");
+		disconnectButton.classList.add("button");
+		disconnectButton.classList.add("button-red");
+		let listItem = document.createElement("li");
+		listItem.appendChild(moduleNameSpan, disconnectButton);
+	}
 }
