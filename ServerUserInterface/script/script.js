cca75b15e915129eda6c12a91f59cfd37d83af9f
microbe-obliterator <billslim0996@gmail.com>
2023-05-05 10:11:45 -0700

    change constructor and add connection manager section control

--- ServerUserInterface/script/script.js
+++ ServerUserInterface/script/script.js
@@ -32,12 +32,16 @@ for (let i of document.querySelectorAll(".file-input")){
   });
 }
 
-let dataStreamPageControl = new (require("./script/DataStreamPageControl.js"))(document.querySelector("#data-stream-module-log > div:last-child > ul"));
+// data stream page control page behaviour
+let dataStreamPageControl = new (require("./script/DataStreamPageControl.js"))();
+
+// connection manager section behaviour
+let connectionManagerPageControl = new (require("./script/ConnectionManagerPageControl.js"))();
 
 // collapsible elements behaviour
 let expandable = document.querySelectorAll(".title-with-expandable");
 for (let i of expandable){
-  let expandButton = i.querySelector("div:first-child > .expand-button");
+  let expandButton = i.querySelector("div:first-child .expand-button");
   expandButton.addEventListener('click', () => {
     i.querySelector("div:last-child").classList.toggle("collapse"); //i fu**ing hate js, whoever designs this progamming language should be reserved a special place in hell next to Satan.
   });
