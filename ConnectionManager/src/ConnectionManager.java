715340ae93fb89da798a90875284b68ab4a765a3
microbe-obliterator <billslim0996@gmail.com>
2023-05-09 19:41:41 -0700

    remove the unnecessary param

--- ConnectionManager/src/ConnectionManager.java
+++ ConnectionManager/src/ConnectionManager.java
@@ -8,7 +8,6 @@ public class ConnectionManager {
             System.err.println("Missing the argument");
             return;
         }
-        args[0] ="{\"moduleName\":\"ConnectionManager\",\"hostAddress\":\"localhost\",\"hostPort\":9999,\"listenerPort\":9998,\"keyStorePath\":\"key.jks\",\"keyStorePassword\":\"hungcom23\",\"databaseUrl\":\"jdbc:postgresql://localhost:5432/authentication?password=hungcom23\",\"poolSize\":2,\"timeout\":5000}";
         JsonObject information = new Gson().fromJson(args[0], JsonObject.class);
         if (!new ChainStartModule(information).execute()) System.exit(2);
     }
