37667bc39187b29a8da2a29faf333bf4d03435aa
microbe-obliterator <billslim0996@gmail.com>
2023-05-20 09:17:36 -0700

    flush the error message to log stream when the received message cannot be tranlated to JSON format

--- ShareResources/src/socket_handler/SocketHandler.java
+++ ShareResources/src/socket_handler/SocketHandler.java
@@ -3,11 +3,12 @@ package socket_handler;
 import chain.Chain;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import system_monitor.MonitorHandler;
 
 import javax.net.ssl.SSLException;
 import java.io.IOException;
 import java.net.SocketException;
-import java.net.SocketTimeoutException;
 
 public abstract class SocketHandler implements Runnable {
     protected final SocketWrapper socket;
@@ -40,8 +41,8 @@ public abstract class SocketHandler implements Runnable {
 //                System.err.println("Socket");
                 break;
             } catch (IOException e) {
-                System.err.println("Fail to read message from socket");
-                e.printStackTrace();
+//                System.err.println("Fail to read message from socket");
+//                e.printStackTrace();
                 break;
             }
             // if a request is null meaning that the other side of the socket is closed
@@ -50,7 +51,17 @@ public abstract class SocketHandler implements Runnable {
             new Thread(() -> {
                 // convert to json object
                 Gson gson = new Gson(); // new gson object for each thread because sharing gson on multiple threads risk processing one object while another one wrest control and use it
-                JsonObject request = gson.fromJson(requestString, JsonObject.class);
+                JsonObject request;
+                try {
+                    request = gson.fromJson(requestString, JsonObject.class);
+                } catch (JsonSyntaxException e){
+                    JsonObject monitorObject = new JsonObject();
+                    monitorObject.addProperty("status", false);
+                    monitorObject.addProperty("notification", "Không thể dịch sang định dạng JSON");
+                    monitorObject.addProperty("request", requestString);
+                    MonitorHandler.addQueue(monitorObject);
+                    return;
+                }
                 // if the request cannot be handled successfully, execute the cleanup chain
                 if (!handleRequest(request).execute()) handleFailure(request).execute();
             }).start();
