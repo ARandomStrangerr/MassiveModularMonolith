d3d4a7ce6642d95a6f2da1c989fa70892d14fafc
microbe-obliterator <billslim0996@gmail.com>
2023-05-23 20:57:31 -0700

    solely run the program by params

--- ViettelEInvoice/src/ViettelEInvoice.java
+++ ViettelEInvoice/src/ViettelEInvoice.java
@@ -1,14 +1,10 @@
 import chain.viettel_einvoice.ChainStartModule;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class ViettelEInvoice {
     public static void main(String[] args) {
-        JsonObject startObj = new JsonObject();
-        startObj.addProperty("moduleName", "ViettelEInvoice");
-        startObj.addProperty("hostAddress", "localhost");
-        startObj.addProperty("hostPort", 9999);
-        startObj.addProperty("jksPath", "key.jks");
-        startObj.addProperty("jksPassword","hungcom23");
-        if (!new ChainStartModule(startObj).execute()) System.exit(2);
+		JsonObject startObject = new Gson().fromJson(args[0], JsonObject.class);
+        if (!new ChainStartModule(startObject).execute()) System.exit(2);
     }
 }
