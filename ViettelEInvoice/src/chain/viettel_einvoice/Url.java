63559ad689c8e19706595790756345eb466b6a80
microbe-obliterator <billslim0996@gmail.com>
2023-05-23 20:56:58 -0700

    API access path

--- ViettelEInvoice/src/chain/viettel_einvoice/Url.java
+++ ViettelEInvoice/src/chain/viettel_einvoice/Url.java
@@ -1,13 +1,13 @@
 package chain.viettel_einvoice;
 
 public enum Url {
-    Authenticate("auth/login"),
-    UploadInvoice("InvoiceAPI/InvoiceWS/createInvoice"),
-	DownloadInvoice("InvoiceAPI/InvoiceUtilsWS/getInvoiceRepresentationFile");
+    Authenticate("https://api-vinvoice.viettel.vn/auth/login"),
+    UploadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceWS/createInvoice"),
+	DownloadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceUtilsWS/getInvoiceRepresentationFile/");
 
     public String path;
 
     Url(String path) {
-        this.path = String.format("%s/%s", "https://api-vinvoice.viettel.vn/services/einvoiceapplication/api", path);
+        this.path = path;
     }
 }
