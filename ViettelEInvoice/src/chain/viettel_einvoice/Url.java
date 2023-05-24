dc908ece916443f8ecad25734a0a1214680f9ead
microbe-obliterator <billslim0996@gmail.com>
2023-05-24 12:14:05 -0700

    add link to upload draft invoice

--- ViettelEInvoice/src/chain/viettel_einvoice/Url.java
+++ ViettelEInvoice/src/chain/viettel_einvoice/Url.java
@@ -1,11 +1,12 @@
 package chain.viettel_einvoice;
 
 public enum Url {
-    Authenticate("https://api-vinvoice.viettel.vn/auth/login"),
-    UploadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceWS/createInvoice"),
+    Authenticate("https://api-vinvoice.viettel.vn/auth/login/"),
+    UploadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceWS/createInvoice/"),
+	UploadDraftInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceWS/createOrUpdateInvoiceDraft/"),
 	DownloadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceUtilsWS/getInvoiceRepresentationFile/");
 
-    public String path;
+    public final String path;
 
     Url(String path) {
         this.path = path;
