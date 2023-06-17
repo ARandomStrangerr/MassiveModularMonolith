741920172d31912c8ed9619abd6d341f6333ab63
microbe-obliterator <billslim0996@gmail.com>
2023-06-17 14:55:10 -0700

    add searching invoice address

--- ViettelEInvoice/src/chain/viettel_einvoice/Url.java
+++ ViettelEInvoice/src/chain/viettel_einvoice/Url.java
@@ -4,7 +4,8 @@ public enum Url {
     Authenticate("https://api-vinvoice.viettel.vn/auth/login/"),
     UploadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceWS/createInvoice/"),
     UploadDraftInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceWS/createOrUpdateInvoiceDraft/"),
-    DownloadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceUtilsWS/getInvoiceRepresentationFile/");
+    DownloadInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceUtilsWS/getInvoiceRepresentationFile/"),
+	SearchInvoice("https://api-vinvoice.viettel.vn/services/einvoiceapplication/api/InvoiceAPI/InvoiceUtilsWS/getInvoices/");
     public final String path;
 
     Url(String path) {
