2022999c24ed731f50f13128df909fbadd1369bd
microbe-obliterator <billslim0996@gmail.com>
2023-06-20 09:17:13 -0700

    new format for the excel file

--- ViettelEInvoice/src/chain/viettel_einvoice/LinkReadFromExcel.java
+++ ViettelEInvoice/src/chain/viettel_einvoice/LinkReadFromExcel.java
@@ -7,6 +7,7 @@ import com.google.gson.JsonObject;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
+import java.util.Objects;
 
 public class LinkReadFromExcel extends Link {
 	public LinkReadFromExcel(Chain chain) {
@@ -28,26 +29,37 @@ public class LinkReadFromExcel extends Link {
 				generalInvoiceInfo.addProperty("templateCode", dataFormatter.formatCellValue(row.getCell(2))); // ký hiệu mẫu hóa đơn
 				generalInvoiceInfo.addProperty("invoiceSeries", dataFormatter.formatCellValue(row.getCell(3))); // ký hiệu hóa đơn
 				generalInvoiceInfo.addProperty("currencyCode", dataFormatter.formatCellValue(row.getCell(4))); // mã tiền tệ
-				generalInvoiceInfo.addProperty("paymentStatus", true);
+				int adjustmentType;
+				try {
+					adjustmentType = Integer.parseInt(dataFormatter.formatCellValue(row.getCell(5)));
+				} catch (Exception e){
+					adjustmentType = 1;
+				}
+				generalInvoiceInfo.addProperty("adjustmentType", adjustmentType); // Trạng thái điều chỉnh hóa đơn
+				generalInvoiceInfo.addProperty("adjustmentInvoiceType", dataFormatter.formatCellValue(row.getCell(6))); // Loại điều chỉnh
+				generalInvoiceInfo.addProperty("originalInvoiceId", dataFormatter.formatCellValue(row.getCell(7))); // số hóa đơn gốc
+				// Thời gian lập hóa đơn gốc going to be in the next step
+				generalInvoiceInfo.addProperty("paymentStatus", dataFormatter.formatCellValue(row.getCell(9))); // trạng thái thanh toán
+				generalInvoiceInfo.addProperty("originalInvoiceType",dataFormatter.formatCellValue(row.getCell(10))); // loại hóa đơn gốc
 				// seller information
 				JsonObject sellerInfo = new JsonObject();
-				sellerInfo.addProperty("sellerLegalName", dataFormatter.formatCellValue(row.getCell(5))); // tên người bán
+				sellerInfo.addProperty("sellerLegalName", dataFormatter.formatCellValue(row.getCell(11))); // tên người bán
 				sellerInfo.addProperty("sellerTaxCode", chain.getProcessObject().get("body").getAsJsonObject().get("username").getAsString()); // mã số thuế bên bán
-				sellerInfo.addProperty("sellerAddressLine", dataFormatter.formatCellValue(row.getCell(6))); // địa chỉ bên bán
+				sellerInfo.addProperty("sellerAddressLine", dataFormatter.formatCellValue(row.getCell(12))); // địa chỉ bên bán
 				// buyer information
 				JsonObject buyerInfo = new JsonObject();
-				buyerInfo.addProperty("buyerName", dataFormatter.formatCellValue(row.getCell(7))); // tên người mua
-				buyerInfo.addProperty("buyerCode", dataFormatter.formatCellValue(row.getCell(8))); // mã khách hàng
-				buyerInfo.addProperty("buyerAddressLine", dataFormatter.formatCellValue(row.getCell(9))); // địa chỉ xuất hóa đơn
+				buyerInfo.addProperty("buyerName", dataFormatter.formatCellValue(row.getCell(13))); // tên người mua
+				buyerInfo.addProperty("buyerCode", dataFormatter.formatCellValue(row.getCell(14))); // mã khách hàng
+				buyerInfo.addProperty("buyerAddressLine", dataFormatter.formatCellValue(row.getCell(15))); // địa chỉ xuất hóa đơn
 				// payment method
 				JsonArray paymentMethod = new JsonArray();
 				JsonObject paymentMethodObj = new JsonObject();
-				paymentMethodObj.addProperty("paymentMethodName", dataFormatter.formatCellValue(row.getCell(10))); // hình thức thanh toán
+				paymentMethodObj.addProperty("paymentMethodName", dataFormatter.formatCellValue(row.getCell(16))); // hình thức thanh toán
 				paymentMethod.add(paymentMethodObj);
 				// item information
 				JsonArray itemInfo = new JsonArray();
 				JsonObject item = new JsonObject();
-				for (int index = 11; index < row.getLastCellNum(); index += 5, item = new JsonObject()) {
+				for (int index = 17; !Objects.equals(dataFormatter.formatCellValue(row.getCell(index)), ""); index += 5, item = new JsonObject()) {
 					item.addProperty("itemName", dataFormatter.formatCellValue(row.getCell(index))); // tên hàng hóa, dịch vụ
 					item.addProperty("unitName", dataFormatter.formatCellValue(row.getCell(index + 1))); // tên đơn vị tính
 					item.addProperty("unitPrice", Long.parseLong(dataFormatter.formatCellValue(row.getCell(index + 2)))); // đơn giá
